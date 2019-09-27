9a06cf1fcd258c8d3249fe62a210eeeef35c5f6e
ErickElizondo <43624905+ErickElizondo@users.noreply.github.com>
2019-09-26 21:10:16 -0600

    Update main.java
    Muestra el contenido de los grupos que contienen algo útil

--- main.java
+++ main.java
@@ -1,21 +1,18 @@
 package caso7;
 
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.Random;
-
 public class main {
 
 	public static void main(String[] args) {
-		final String textoEncriptado="xZwM7BWIpSjYyGFr9rhpEa+cYVtACW7yQKmyN6OYSCv0ZEg9jWbc6lKzzCxRSSIvOvlimQZBMZOYnOwiA9yy3YU8zk4abFSItoW6Wj0ufQ0=";
-		final String primerParteLlave="29dh120";
-		final String segundaParteLlave="dk1";
-		final String tercerParteLlave="3";
+		final String TextoEncriptado="xZwM7BWIpSjYyGFr9rhpEa+cYVtACW7yQKmyN6OYSCv0ZEg9jWbc6lKzzCxRSSIvOvlimQZBMZOYnOwiA9yy3YU8zk4abFSItoW6Wj0ufQ0=";
+		final String PrimerParteLlave="29dh120";
+		final String SegundaParteLlave="dk1";
+		final String TercerParteLlave="3";
 		LinkedList<grupo> listaGrupos=new LinkedList<grupo>();
 		char listaLetras[]= { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
 			      's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 		char listaNum[]= { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+		LinkedList<Integer> gruposExito=new LinkedList<>();		
 		for(int i=0;i<26;i++) {
 			grupo nuevoAInsertar=new grupo();
 			while(nuevoAInsertar.letrasSeleccionadas.size()<5) {
@@ -26,27 +23,27 @@ public class main {
 			}
 			listaGrupos.add(nuevoAInsertar);
 		}
-		int contador=0;
 		for(int i=0;i<20;i++) {
 			String probar="";
 			for(char letra:listaGrupos.get(i).letrasSeleccionadas) {
 				for(char num:listaGrupos.get(i).numerosSeleccionados) {
-					probar=primerParteLlave+letra+segundaParteLlave+num+tercerParteLlave;
-					contador++;
-					//System.out.println(desEncriptar.decrypt(textoEncriptado, probar));					
+					probar=primerParteLlave+letra+segundaParteLlave+num+tercerParteLlave;			
 					String respuesta=desEncriptar.decrypt(textoEncriptado, probar);
 					if(!(respuesta==null)) {
-						System.out.println(contador);
-						System.out.println(respuesta);
-						return;
+						gruposExito.add(i);
+						
+						
 					}
 				}
 			}
-		}
-
-		
+		}		
+		System.out.println(gruposExito.toString());//Indices de los grupos útiles
+		grupo union=new grupo();
+		for(int i=0;i<gruposExito.size();i++) {			
+			System.out.println(listaGrupos.get(gruposExito.get(i)).letrasSeleccionadas.toString());
+			System.out.println(listaGrupos.get(gruposExito.get(i)).numerosSeleccionados.toString());
+		}	
 		
 	}
-	
 
 }
