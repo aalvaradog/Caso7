d2e1ee441ae6fa413720a6e8fbc46194665050b8
ErickElizondo <43624905+ErickElizondo@users.noreply.github.com>
2019-09-26 19:42:05 -0600

    Update grupo.java

--- grupo.java
+++ grupo.java
@@ -1,5 +1,12 @@
 package caso7;
-
+import java.util.*;
 public class grupo {
-
+	HashSet<Character> letrasSeleccionadas;
+	HashSet<Character> numerosSeleccionados;
+	//Clase utilizada para tener una lista de números y letras de cada subgrupo de las combinaciones de letras y números
+	grupo(){
+		this.letrasSeleccionadas=new HashSet<>();
+		this.numerosSeleccionados=new HashSet<>();
+	}
+	
 }
